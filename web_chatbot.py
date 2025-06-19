25536d2a133df2b1e290f500af9c8322a35980b4
lchan3 <linda.chan2@health.nsw.gov.au>
2025-06-20 08:23:38 +1000

    Update web_chatbot.py

--- web_chatbot.py
+++ web_chatbot.py
@@ -57,8 +57,5 @@ def chatbot():
     return render_template_string(html_template, response=response)
 
 if __name__ == "__main__":
-  
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
